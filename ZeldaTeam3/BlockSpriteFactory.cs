5bf178ef122bbbf528c0b8110931f4164695887b
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-09-18 00:19:00 -0400

    All constructors now new sprite constructs, animated fire constructor is a shot in the dark, pls review

--- ZeldaTeam3/BlockSpriteFactory.cs
+++ ZeldaTeam3/BlockSpriteFactory.cs
@@ -29,145 +29,143 @@ namespace Zelda
 
         public ISprite CreateTopWall()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(0, 0, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(0,0));
         }
 
         public ISprite CreateLeftWall()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(0, 32, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(0, 32));
         }
 
         public ISprite CreateRightWall()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(0, 64, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(0, 64));
         }
 
         public ISprite CreateBottomWall()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(0, 96, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(0, 96));
         }
 
         public ISprite CreateTopOpenDoor()
         {
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(32, 0));
             return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(32, 0, 32, 32));
         }
 
         public ISprite CreateLeftOpenDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(32, 32, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(32, 32));
         }
 
         public ISprite CreateRightOpenDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(32, 64, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(32, 64));
         }
 
         public ISprite CreateBottomOpenDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(32, 96, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(32, 96));
         }
 
         public ISprite CreateTopLockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(64, 0, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(64, 0));
         }
 
         public ISprite CreateLeftLockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(64, 32, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(64, 32));
         }
 
         public ISprite CreateRightLockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(64, 64, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(64, 64));
         }
 
         public ISprite CreateBottomLockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(64, 96, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(64, 96));
         }
 
         public ISprite CreateTopBlockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(96, 0, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(96, 0));
         }
 
         public ISprite CreateLeftBlockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(96, 32, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(96, 32));
         }
 
         public ISprite CreateRightBlockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(96, 64, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(96, 64));
         }
 
         public ISprite CreateBottomBlockedDoor()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(96, 96, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(96, 96));
         }
 
         public ISprite CreateTopWallHole()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(128, 0, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(128, 0));
         }
 
         public ISprite CreateLeftWallHole()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(128, 32, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(128, 32));
         }
 
         public ISprite CreateRightWallHole()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(128, 64, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(128, 64));
         }
 
         public ISprite CreateBottomWallHole()
         {
-            return new SpriteFixedStatic(_doorSpritesheet, new Rectangle(128, 96, 32, 32));
+            return new Sprite(Texture2D _doorSpritesheet, 32, 32, 1, new Point(128, 96));
         }
 
         public ISprite CreateSolidBlock()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(0, 0, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(0, 0));
         }
 
         public ISprite CreateGapTile()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(16, 0, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(16, 0));
         }
 
         public ISprite CreateStatue1()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(0, 16, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(0, 16));
         }
 
         public ISprite CreateStatue2()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(16, 16, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(16, 16));
         }
 
         public ISprite CreateStairs1()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(0, 32, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(0, 32));
         }
 
         public ISprite CreateStairs2()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(0, 48, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(0, 48));
         }
 
         public ISprite CreateBrickBlock()
         {
-            return new SpriteFixedStatic(_tileSpritesheet, new Rectangle(16, 48, 16, 16));
+            return new Sprite(Texture2D _tileSpritesheet, 16, 16, 1, new Point(16, 48));
         }
 
         public ISprite CreateFire()
         {
-            Rectangle _fireFrameOne = new Rectangle(0, 60, 16, 16);
-            Rectangle _fireFrameTwo = new Rectangle(16, 60, 16, 16);
-            Rectangle[] _frameBounceArray = [_fireFrameOne, _fireFrameTwo];
-            return new SpriteFixedAnimated(_tileSpritesheet, _frameBounceArray);
+            return new (Texture2D _tileSpritesheet, 32, 16, 2, new Point(0,60), frameDelay: 15, paletteRowCount: 1, paletteRows: 2);
         }
     }
 }
