67dee00ff810f4b19b17ba895b24a6803606302d
Chase Colman <chase@colman.io>
2019-09-27 13:47:09 -0400

    Sprite: Pull out frame delay mechanism and document

---
+++ ZeldaTeam3/FrameDelay.cs
@@ -0,0 +1,31 @@
+﻿namespace Zelda
+{
+    class FrameDelay
+    {
+        private readonly int _frames;
+        private bool _paused;
+        private int _framesDelayed;
+
+        public bool Delayed => _framesDelayed != 0 || _paused;
+
+        public FrameDelay(int frames)
+        {
+            _frames = frames;
+        }
+
+        public void Pause()
+        {
+            _paused = true;
+        }
+
+        public void Resume()
+        {
+            _paused = false;
+        }
+
+        public void Update()
+        {
+            if (!_paused && _framesDelayed++ == _frames) _framesDelayed = 0;
+        }
+    }
+}
