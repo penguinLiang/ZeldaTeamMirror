b509f18f803e0e5d8f8c7472f931a0e5145b4832
Chase Colman <chase@colman.io>
2019-10-04 21:54:39 -0400

    Add ICollideable

---
+++ ZeldaTeam3/ICollideable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Zelda
+{
+    interface ICollideable
+    {
+        bool CollidesWith(Rectangle rect);
+        ICommand PlayerEffect(IPlayer player);
+        ICommand EnemyEffect(IEnemy enemy);
+        ICommand ProjectileEffect();
+    }
+}
