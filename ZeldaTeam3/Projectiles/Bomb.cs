3778c53a56414ec93e2adb89e16badd3e6767c05
fink-255 <fink.255@buckeyemail.osu.edu>
2019-10-05 19:14:19 -0400

    Added and implemented BombExplosion class to handle multiple bomb explosion sprites and to eventually handle explosion collision with walls. Modified Bomb.cs accordingly.

--- ZeldaTeam3/Projectiles/Bomb.cs
+++ ZeldaTeam3/Projectiles/Bomb.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Zelda.Projectiles
@@ -6,11 +8,12 @@ namespace Zelda.Projectiles
     public class Bomb : IDrawable
     {
         private const int FramesToExplosion = 100;
-        private const int FramesToDisappear = 140;
+        private const int FramesToDisappear = 160;
 
         private readonly Vector2 _location;
         private readonly SpriteBatch _spriteBatch;
         private ISprite _sprite;
+        private BombExplosion _explosion;
         private int _framesDelayed;
 
         public Bomb(SpriteBatch spriteBatch, Vector2 location)
@@ -23,18 +26,25 @@ namespace Zelda.Projectiles
         public void Update()
         {
             _sprite.Update();
+            
             if (++_framesDelayed == FramesToExplosion)
             {
-                _sprite = ProjectileSpriteFactory.Instance.CreateBombExplosion();
-            } else if (_framesDelayed == FramesToDisappear)
-            {
+                _explosion = new BombExplosion(_spriteBatch, new Vector2(_location.X - 16, _location.Y - 16));
                 _sprite.Hide();
+            }
+            if (_framesDelayed >= FramesToExplosion)
+            {
+                _explosion.Update();
             }
         }
 
         public void Draw()
         {
             _sprite.Draw(_spriteBatch, _location);
+            if (_framesDelayed >= FramesToExplosion)
+            {
+                _explosion.Draw();
+            }
         }
     }
 }
---
+++ ZeldaTeam3/Projectiles/BombExplosion.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Zelda.Projectiles
+{
+    public class BombExplosion : IDrawable
+    {
+        private const int FramesToDisappear = 60;
+        private const int NumberOfSprites = 4;
+
+        private readonly Vector2 _location;
+        private readonly SpriteBatch _spriteBatch;
+        private Vector2[] _spriteLocations = new Vector2[NumberOfSprites];
+        private ISprite[] _sprites = new ISprite[NumberOfSprites];
+        private int _framesDelayed;
+
+        private void SetSmokeCloudLocations()
+        {
+            Random rand = new Random();
+
+            _spriteLocations[0] = new Vector2(_location.X + 16, _location.Y + 16);
+            if (rand.Next(2) == 0)
+            {
+                _spriteLocations[1] = new Vector2(_location.X + 8, _location.Y);
+                _spriteLocations[2] = new Vector2(_location.X + 32, _location.Y + 16);
+                _spriteLocations[3] = new Vector2(_location.X + 24, _location.Y + 32);
+            }
+            else
+            {
+                _spriteLocations[1] = new Vector2(_location.X + 24, _location.Y);
+                _spriteLocations[2] = new Vector2(_location.X, _location.Y + 16);
+                _spriteLocations[3] = new Vector2(_location.X + 8, _location.Y + 32);
+            }
+        }
+
+        public BombExplosion(SpriteBatch spriteBatch, Vector2 location)
+        {
+            _location = location;
+            _spriteBatch = spriteBatch;
+            SetSmokeCloudLocations();
+            for (int i = 0; i < NumberOfSprites; i++)
+            {
+                _sprites[i] = ProjectileSpriteFactory.Instance.CreateBombExplosion();
+            }
+        }
+
+        public void Update()
+        {
+            if (_framesDelayed++ == FramesToDisappear)
+            {
+                foreach (var sprite in _sprites)
+                {
+                    sprite.Hide();
+                }
+            }
+            foreach (var sprite in _sprites)
+            {
+                sprite.Update();
+            }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < NumberOfSprites; i++)
+            {
+                _sprites[i].Draw(_spriteBatch, _spriteLocations[i]);
+            }
+        }
+    }
+}
