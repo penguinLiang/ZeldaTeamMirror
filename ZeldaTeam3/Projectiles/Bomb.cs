f65776463970ad5a73574be2cef96aeccdf46c28
fink-255 <fink.255@buckeyemail.osu.edu>
2019-10-05 21:31:17 -0400

    Refactored bomb explosion into Bomb.cs. Excluded BombExplosion.cs from project. The private helper method to randomly choose the outer sprite locations is now called in Update() instead of the constructor. Only one sprite now exists, but it is drawn four times at different locations.

--- ZeldaTeam3/Projectiles/Bomb.cs
+++ ZeldaTeam3/Projectiles/Bomb.cs
@@ -8,11 +8,13 @@ namespace Zelda.Projectiles
     public class Bomb : IDrawable
     {
         private const int FramesToExplosion = 100;
+        private const int FramesToDisappear = 160;
+        private const int NumberOfOuterExplosionSprites = 3;
 
         private readonly Vector2 _location;
         private readonly SpriteBatch _spriteBatch;
+        private Vector2[] _outerExplosionSpriteLocations = new Vector2[NumberOfOuterExplosionSprites];
         private ISprite _sprite;
-        private BombExplosion _explosion;
         private int _framesDelayed;
 
         public Bomb(SpriteBatch spriteBatch, Vector2 location)
@@ -22,18 +24,35 @@ namespace Zelda.Projectiles
             _spriteBatch = spriteBatch;
         }
 
+        private void SetExplosionSpriteLocations()
+        {
+            Random rand = new Random();
+            if (rand.Next(2) == 0)
+            {
+                _outerExplosionSpriteLocations[0] = new Vector2(_location.X - 8, _location.Y - 16);
+                _outerExplosionSpriteLocations[1] = new Vector2(_location.X + 16, _location.Y);
+                _outerExplosionSpriteLocations[2] = new Vector2(_location.X + 8, _location.Y + 16);
+            }
+            else
+            {
+                _outerExplosionSpriteLocations[0] = new Vector2(_location.X + 8, _location.Y - 16);
+                _outerExplosionSpriteLocations[1] = new Vector2(_location.X - 16, _location.Y);
+                _outerExplosionSpriteLocations[2] = new Vector2(_location.X - 8, _location.Y + 16);
+            }
+        }
+
         public void Update()
         {
             _sprite.Update();
             
             if (++_framesDelayed == FramesToExplosion)
             {
-                _explosion = new BombExplosion(_spriteBatch, new Vector2(_location.X - 16, _location.Y - 16));
-                _sprite.Hide();
+                _sprite = ProjectileSpriteFactory.Instance.CreateBombExplosion();
+                SetExplosionSpriteLocations();
             }
-            if (_framesDelayed >= FramesToExplosion)
+            else if (_framesDelayed == FramesToDisappear)
             {
-                _explosion.Update();
+                _sprite.Hide();
             }
         }
 
@@ -42,7 +61,10 @@ namespace Zelda.Projectiles
             _sprite.Draw(_spriteBatch, _location);
             if (_framesDelayed >= FramesToExplosion)
             {
-                _explosion.Draw();
+                foreach (var spriteLocation in _outerExplosionSpriteLocations)
+                {
+                    _sprite.Draw(_spriteBatch, spriteLocation);
+                }
             }
         }
     }
