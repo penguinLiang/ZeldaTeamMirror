a0c6337551cc9387e73141331f393a875bb38448
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-10-06 14:28:54 -0400

    Fairy and Heart implementation

---
+++ ZeldaTeam3/Commands/NullCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Zelda.Commands
+{
+    internal class NullCommand : ICommand
+    {
+        private readonly IPlayer _link;
+
+        public NullCommand()
+        {
+
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public override string ToString() => "Filler Command. Nothing happens.";
+    }
+}
