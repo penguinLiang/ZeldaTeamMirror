17a699cc419ac57cbb968d49a93d36c63e164bb2
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-10-14 01:25:08 -0400

    Added update for JumpMap

--- ZeldaTeam3/JumpMap.cs
+++ ZeldaTeam3/JumpMap.cs
@@ -8,11 +8,22 @@ namespace Zelda
 {
     public class JumpMap
     {
-        Texture image;
+        private Texture2D image;
+        private SpriteBatch _spriteBatch;
+        private ContentManager _content;
 
-        public JumpMap(ContentManager content)
+
+        public JumpMap(SpriteBatch spriteBatch, ContentManager content)
         {
+            _spriteBatch = spriteBatch;
+            _content = content;
             image = content.Load<Texture2D>("JumpMap");
+            
+        }
+
+        public Update()
+        {
+            _spriteBatch.Draw(image, new Rectangle(0, 0, 512, 448), Color.White);
         }
     }
 }
