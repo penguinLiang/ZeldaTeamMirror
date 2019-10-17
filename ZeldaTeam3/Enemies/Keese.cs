98cf6c4f1b5fdecfb4a3e059fb2f5a48a4dea1cb
Chase Colman <chase@colman.io>
2019-10-16 21:22:20 -0400

    No-Op Keese Halt for testing purposes

--- ZeldaTeam3/Enemies/Keese.cs
+++ ZeldaTeam3/Enemies/Keese.cs
@@ -32,5 +32,10 @@ namespace Zelda.Enemies
         {
             _agent.Update();
         }
+
+        public override void Halt()
+        {
+            // NO-OP: Keese fly through walls!
+        }
     }
 }
