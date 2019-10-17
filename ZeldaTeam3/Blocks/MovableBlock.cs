500f3a2e47fb552324f113df61574f071c5c0201
fink-255 <fink.255@buckeyemail.osu.edu>
2019-10-16 22:13:41 -0400

    MovableBlock now handles collision. Collision with a player triggers the block's own movement because ICollideable does not account for movable blocks.

--- ZeldaTeam3/Blocks/MovableBlock.cs
+++ ZeldaTeam3/Blocks/MovableBlock.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using Microsoft.Xna.Framework;
 using Zelda.Commands;
 
@@ -8,11 +8,19 @@ namespace Zelda.Blocks
     internal class MovableBlock : ICollideable, IDrawable
     {
         private readonly ISprite _sprite = BlockSpriteFactory.Instance.CreateSolidBlock();
-        public Rectangle Bounds { get; }
+        public Point Location { get; private set; }
+        public Rectangle Bounds { get; private set; }
+
+        private Direction _pushDirection;
+        private int _distanceMoved;
+
+        private bool _unmoved = true;
+        private bool _moving;
 
         public MovableBlock(Point location)
         {
-            Bounds = new Rectangle(location, new Point(16, 16));
+            Location = location;
+            Bounds = new Rectangle(location.X, location.Y, 16, 16);
         }
 
         public bool CollidesWith(Rectangle rect)
@@ -20,48 +28,96 @@ namespace Zelda.Blocks
             return Bounds.Intersects(rect);
         }
 
+        private bool TrySetBlockDirection(Rectangle playerBounds)
+        {
+            Rectangle overlap = Rectangle.Intersect(Bounds, playerBounds);
+            if (overlap.Width > overlap.Height)
+            {
+                if (Bounds.Y < playerBounds.Y)
+                {
+                    _pushDirection = Direction.Up;
+                }
+                else
+                {
+                    _pushDirection = Direction.Down;
+                }
+            }
+            else if (overlap.Width < overlap.Height)
+            {
+                if (Bounds.X < playerBounds.X)
+                {
+                    _pushDirection = Direction.Left;
+                }
+                else
+                {
+                    _pushDirection = Direction.Right;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
         public ICommand PlayerEffect(IPlayer player)
         {
-            return NoOp.Instance;
+            if (_unmoved && TrySetBlockDirection(player.Bounds))
+            {
+                _moving = true;
+                _unmoved = false;
+            }
+            return new MoveableHalt(player);
         }
 
         public ICommand EnemyEffect(IEnemy enemy)
         {
-            return NoOp.Instance;
+            return new MoveableHalt(enemy);
         }
 
         public ICommand ProjectileEffect(IHaltable projectile)
         {
-            return NoOp.Instance;
+            return new MoveableHalt(projectile);
         }
 
         public void Update()
         {
+            if (_moving)
+            {
+                switch (_pushDirection)
+                {
+                    case Direction.Up:
+                        Location = new Point(Location.X, Location.Y - 1);
+                        Bounds = new Rectangle(Location.X, Location.Y, 16, 16);
+                        break;
+                    case Direction.Down:
+                        Location = new Point(Location.X, Location.Y + 1);
+                        Bounds = new Rectangle(Location.X, Location.Y, 16, 16);
+                        break;
+                    case Direction.Left:
+                        Location = new Point(Location.X - 1, Location.Y);
+                        Bounds = new Rectangle(Location.X, Location.Y, 16, 16);
+                        break;
+                    case Direction.Right:
+                        Location = new Point(Location.X + 1, Location.Y);
+                        Bounds = new Rectangle(Location.X, Location.Y, 16, 16);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                if (++_distanceMoved >= 16)
+                {
+                    _moving = false;
+                }
+            }
+
             _sprite.Update();
         }
 
         public void Draw()
         {
-            _sprite.Draw(Bounds.Location.ToVector2());
-        }
-
-        public void MoveLeft()
-        {
-            return; 
-        }
-
-        public void MoveRight()
-        {
-            return;
-        }
-
-        public void MoveDown()
-        {
-            return;
-        }
-        public void MoveUp()
-        {
-            return;
+            _sprite.Draw(Location.ToVector2());
         }
     }
 }
