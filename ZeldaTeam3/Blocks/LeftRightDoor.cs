55a1f752fc914aed65a3bc2b62c543bc00781370
qshaner <quinn.shaner@gmail.com>
2019-10-18 13:26:35 -0400

    split up doors and stairs, has been tested

---
+++ ZeldaTeam3/Blocks/LeftRightDoor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Zelda.Commands;
+
+namespace Zelda.Blocks
+{
+    class LeftRightDoor : ICollideable, IDrawable, IActivatable
+    {
+        private readonly ISprite _sprite;
+        public Rectangle Bounds { get; private set; }
+
+        private readonly Vector2 _drawLocation;
+
+        public LeftRightDoor(Point location, BlockType block)
+        {
+            Bounds = new Rectangle(location.X, location.Y, 32, 48);
+            _sprite = BlockTypeSprite.Sprite(block);
+            _drawLocation = new Vector2(location.X, location.Y + 8);
+                  }
+
+        public bool CollidesWith(Rectangle rect)
+        {
+            return Bounds.Intersects(rect);
+        }
+
+        public ICommand PlayerEffect(IPlayer player)
+        {
+            return new LinkKnockback(player);
+        }
+
+        public ICommand EnemyEffect(IEnemy enemy)
+        {
+            return new MoveableHalt(enemy);
+        }
+
+        public ICommand ProjectileEffect(IHaltable projectile)
+        {
+            return new MoveableHalt(projectile);
+        }
+
+        public void Update()
+        {
+            _sprite?.Update();
+        }
+
+        public void Draw()
+        {
+            _sprite?.Draw(_drawLocation);
+        }
+
+        public void Activate()
+        {
+            return;
+        }
+    }
+}
