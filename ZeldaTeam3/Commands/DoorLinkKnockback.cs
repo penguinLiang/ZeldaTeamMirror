db0804aa5ab618c274899ee9718098e0f3338be1
Chase Colman <chase@colman.io>
2019-10-18 22:42:44 -0400

    Fix knockback regression

--- ZeldaTeam3/Commands/DoorLinkKnockback.cs
+++ ZeldaTeam3/Commands/DoorLinkKnockback.cs
@@ -1,16 +1,17 @@
 ﻿namespace Zelda.Commands
 {
-    internal class LinkKnockback : ICommand
+    internal class DoorLinkKnockback : ICommand
     {
         private readonly IPlayer _link;
 
-        public LinkKnockback(IPlayer link)
+        public DoorLinkKnockback(IPlayer link)
         {
             _link = link;
         }
 
         public void Execute()
         {
+            _link.Halt();
             _link.Knockback();
         }
         public override string ToString() => "Link: Knockback";
