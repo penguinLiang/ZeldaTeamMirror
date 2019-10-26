adbbe5aabbc9bbb27c14f0cf838c27f84bb840be
fink-255 <fink.255@buckeyemail.osu.edu>
2019-10-26 18:37:26 -0400

    Implemented fireball projectile, including collision. Modified ProjectileSpriteFactory to include one sprite for Old Man Fireballs and another for Aquamentus Fireballs.

---
+++ ZeldaTeam3/Projectiles/Fireball.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Zelda.Projectiles
+{
+    internal class Fireball : ICollideable, IDrawable
+    {
+        private const int FramesToDisappear = 140;
+
+        private Vector2 _location;
+        private Vector2 _velocity;
+        private readonly ISprite _sprite;
+
+        private int _framesDelayed;
+
+        public Rectangle Bounds { get; private set; }
+
+        public Fireball(Point location, Vector2 velocity, bool fromAquamentus)
+        {
+            _location = location.ToVector2();
+            Bounds = new Rectangle(location.X + 4, location.Y + 4, 8, 8);
+            _velocity = velocity;
+            _framesDelayed = 0;
+            if (fromAquamentus)
+            {
+                _sprite = ProjectileSpriteFactory.Instance.CreateAquamentusFireball();
+            }
+            else
+            {
+                _sprite = ProjectileSpriteFactory.Instance.CreateOldManFireball();
+            }
+        }
+
+        public bool CollidesWith(Rectangle rectangle)
+        {
+            return Bounds.Intersects(rectangle);
+        }
+
+        public ICommand PlayerEffect(IPlayer player)
+        {
+            return new Commands.SpawnableDamage(player);
+        }
+
+        public ICommand EnemyEffect(IEnemy enemy)
+        {
+            return Commands.NoOp.Instance;
+        }
+
+        public ICommand ProjectileEffect(IHaltable projectile)
+        {
+            return Commands.NoOp.Instance;
+        }
+
+        public void Update()
+        {
+            _location = Vector2.Add(_location, _velocity);
+            if (_framesDelayed++ >= FramesToDisappear)
+            {
+                _sprite.Hide();
+            }
+            _sprite.Update();
+        }
+
+        public void Draw()
+        {
+            _sprite.Draw(_location);
+        }
+    }
+}
