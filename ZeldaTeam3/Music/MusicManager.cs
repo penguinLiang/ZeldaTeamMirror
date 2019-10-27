12ff25fd4cf7cd1cd9313e81473437a1d2c74753
qshaner <quinn.shaner@gmail.com>
2019-10-27 10:15:23 -0400

    basics

--- ZeldaTeam3/Music/MusicManager.cs
+++ ZeldaTeam3/Music/MusicManager.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Zelda.Music
 {
     class MusicManager
     {
+
+      private SoundEffect _playLabryinthMusic;
+      private SoundEffect _playGameOverMusic;
+      private SoundEffect _playWinMusic;
+
+        public void LoadAllSounds(ContentManager Content)
+        {
+            _playLabryinthMusic = Content.Load<SoundEffect>("");
+            _playGameOverMusic = Content.Load<SoundEffect>("");
+            _playWinMusic = Content.Load<SoundEffect>("");
+
+        }
+
+        public void PlayLabryinthMusic()
+        {
+            
+        }
+
+        public void PlayGameOverMusic() {
+
+        }
+
+        public void PlayWinMusic()
+        {
+
+        }
     }
 }
