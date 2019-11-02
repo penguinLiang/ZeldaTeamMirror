0c528c4be5b29d1bda0a2637cbd0cc4db923c317
qshaner <quinn.shaner@gmail.com>
2019-11-02 11:26:39 -0400

    First pass at IProjectile

---
+++ ZeldaTeam3/Projectiles/AquamentusAttack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+namespace Zelda.Projectiles
+{
+  public class AquamentusAttack: IProjectile
+    {
+      public List<IProjectile> Projectiles { get; set; }
+        public void AddProjectile() { }
+
+        public void removeProjectile() { }
+
+      public  Rectangle Bounds { get; }
+
+        public bool CollidesWith(Rectangle rect) {
+            new NotImplementedException();
+            return false;
+        }
+
+     public   ICommand PlayerEffect(IPlayer player)
+        {
+            new NotImplementedException();
+            return null;
+        }
+      public  ICommand EnemyEffect(IEnemy enemy)
+        {
+            new NotImplementedException();
+            return null;
+        }
+      public  ICommand ProjectileEffect(IHaltable projectile)
+        {
+            new NotImplementedException();
+            return null;
+        }
+
+
+        public void Draw()
+        {
+            new NotImplementedException();
+            
+        }
+
+        public void Update()
+        {
+            new NotImplementedException();
+       
+        }
+
+        public void Knockback()
+        {
+
+        }
+
+        public void Halt()
+        {
+
+        }
+
+    }
+}
---
+++ ZeldaTeam3/Projectiles/IProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+namespace Zelda.Projectiles
+{
+    public interface IProjectile:  ICollideable, IDrawable, IHaltable
+    {
+        List<IProjectile> Projectiles { get; set; }
+        void AddProjectile();
+        void removeProjectile();
+
+    }
+}
+
+//need to be able to get the projectiles
+
+//list the projectiles by type?
+
+// scene should use this
+//secondary item agent should use
+//enemy should use
+
+//Halt destroys projectile
+//
