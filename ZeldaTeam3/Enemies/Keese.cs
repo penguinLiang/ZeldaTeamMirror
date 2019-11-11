a33e0241e6825809dc098a9d38e1fd77f0679df8
it1190 <stneveadomi@yahoo.com>
2019-11-11 00:13:55 -0500

    Clean up Keese.

--- ZeldaTeam3/Enemies/Keese.cs
+++ ZeldaTeam3/Enemies/Keese.cs
@@ -12,8 +12,10 @@ namespace Zelda.Enemies
         protected override ISprite Sprite => _sprite;
 
         private readonly Point _origin;
-        private uint _movementClock;
+        private int _movementClock;
         private int _movementPauseClock;
+        private Point _playerLocation;
+        private Point _nextDestination;
 
         public Keese(Point location)
         {
@@ -33,52 +35,75 @@ namespace Zelda.Enemies
             // NO-OP: Flies through walls
         }
 
-        // Moves in a figure 8
-        private void AdvanceLocation()
+        private void ExecuteAction()
         {
-            if (_movementPauseClock > 0)
+            if (_movementPauseClock-- > 0)
             {
                 _movementPauseClock--;
                 return;
             }
 
-            // ReSharper disable once SwitchStatementMissingSomeCases (Value reset after 80)
-            switch (_movementClock / 10)
+            if (_movementClock % Rng.Next(1,10) == 0)
             {
-                case 0:
-                case 2:
-                    Location.X += 2;
-                    break;
-                case 1: 
-                    Location.X += 2;
-                    Location.Y += 2;
-                    break;
-                case 3:
-                case 7:
-                    Location.Y -= 2;
-                    break;
-                case 4:
-                case 6:
-                    Location.X -= 2;
-                    break;
-                case 5:
-                    Location.X -= 2;
-                    Location.Y += 2;
-                    break;
+                generateNextDestination();
             }
 
-            if (_movementClock == 0 || _movementClock == 40 && Rng.Next(0, 10) < 7)
+            if (_movementClock > 0)
             {
-                _movementPauseClock = Rng.Next(20, 60);
+                _movementClock--;
+                AdvanceToDestination();
             }
+            else
+            {
+                generateNextDestination();
+                _movementClock = Rng.Next(60,100);
+                _movementPauseClock = Rng.Next(30,70);
+            }
+            
+        }
 
-            _movementClock = (_movementClock + 1) % 80;
+        private void AdvanceToDestination()
+        {
+            double xDiff = _playerLocation.X - _nextDestination.X;
+            double yDiff = _playerLocation.Y - _nextDestination.Y;
+            double magnitude = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+            double xScale = _movementClock / 24;
+            double yScale = _movementClock / 24;
+
+            var normalizedY = yDiff / magnitude * xScale;
+            var normalizedX = xDiff / magnitude * yScale;
+            
+            Location.X -= (int)normalizedX;
+            Location.Y -= (int)normalizedY;
+        }
+
+
+        public override void Target(Point playerLocation)
+        {
+            _playerLocation = playerLocation;
+        }
+
+        private void generateNextDestination()
+        {
+            const float locationScale = 2f;
+            double xDiff = _playerLocation.X - Location.X;
+            double yDiff = _playerLocation.Y - Location.Y;
+            if (Rng.Next(3) == 2)
+            {
+                _nextDestination = new Point((int)(Location.X + Rng.Next(-100, 100)), (int)(Location.Y + Rng.Next(-100, 100)));
+            }
+            else
+            {
+                _nextDestination = new Point((int)(Location.X + xDiff * locationScale), (int)(Location.Y + yDiff * locationScale));
+            }
+            
+
         }
 
         public override void Update()
         {
             if (Alive && CanMove)
-                AdvanceLocation();
+                ExecuteAction();
             base.Update();
         }
     }
