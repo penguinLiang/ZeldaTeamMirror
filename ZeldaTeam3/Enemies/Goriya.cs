b2e816463dfa6f1ce3bc09c0a21dc6cb275a56b7
it1190 <stneveadomi@yahoo.com>
2019-11-11 18:43:25 -0500

    Fix knockback for Stalfos and Goriya.

--- ZeldaTeam3/Enemies/Goriya.cs
+++ ZeldaTeam3/Enemies/Goriya.cs
@@ -102,7 +102,6 @@ namespace Zelda.Enemies
         {
             _agentStatus = AgentState.Knocked;
             _agentClock = ActionDelay / 2;
-            FlipDirection();
         }
 
         public override void Halt()
@@ -133,12 +132,11 @@ namespace Zelda.Enemies
                 case AgentState.Knocked:
                     if (_agentClock == 0)
                     {
-                        FlipDirection();
                         _agentStatus = AgentState.Ready;
                     }
                     else
                     {
-                        Move(_statusDirection);
+                        Move(DirectionUtility.Flip(_statusDirection));
                     }
                     break;
                 case AgentState.Moving:
