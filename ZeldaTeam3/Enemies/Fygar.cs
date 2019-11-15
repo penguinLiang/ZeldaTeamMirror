f26fb11e50af2b865e42d37726b5b7f1ebf55cbd
it1190 <stneveadomi@yahoo.com>
2019-11-14 21:24:33 -0500

    Implemented Fygar.

---
+++ ZeldaTeam3/Enemies/Fygar.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.Xna.Framework;
+using Zelda.Commands;
+using Zelda.Projectiles;
+
+namespace Zelda.Enemies
+{
+    public class Fygar : EnemyAgent
+    {
+        private const int ActionDelay = 16;
+
+        private static readonly Point Size = new Point(13, 13);
+        public override Rectangle Bounds => new Rectangle(Location, Alive ? Size : Point.Zero);
+        private ISprite _sprite;
+        protected override ISprite Sprite => _sprite;
+        private static readonly List<AgentState> ValidAgentStates = new List<AgentState>
+        {
+            AgentState.Ready,
+            AgentState.Moving,
+            AgentState.Halted,
+            AgentState.Attacking
+        };
+
+        private readonly Point _origin;
+
+        private int _agentClock;
+        private Direction _currentDirection;
+        private AgentState _agentStatus;
+        private Point _playerLocation;
+        private Direction _facing;
+
+        public Fygar(Point location)
+        {
+            _origin  = location;
+        }
+
+        public override void Spawn()
+        {
+            base.Spawn();
+
+            _sprite = EnemySpriteFactory.Instance.CreateFygarFaceLeft();
+            _facing = Direction.Left;
+            Location = _origin;
+            Health = 2;
+            _agentStatus = AgentState.Ready;
+            _currentDirection = Direction.Down;
+        }
+
+        private void FlipDirection()
+        {
+            _currentDirection = DirectionUtility.Flip(_currentDirection);
+        }
+
+        protected override void Move(Direction direction)
+        {
+            base.Move(direction);
+            if (direction == Direction.Right)
+            {
+                _sprite = EnemySpriteFactory.Instance.CreateFygarFaceRight();
+                _facing = Direction.Right;
+            }
+            else if(direction == Direction.Left)
+            {
+                _sprite = EnemySpriteFactory.Instance.CreateFygarFaceLeft();
+                _facing = Direction.Left;
+            }
+        }
+
+        protected override void Knockback()
+        {
+            _agentStatus = AgentState.Knocked;
+            _agentClock = ActionDelay / 2;
+        }
+
+        public override void Halt()
+        {
+            _agentStatus = AgentState.Halted;
+            _agentClock = ActionDelay;
+            FlipDirection();
+            Move(_currentDirection);
+        }
+
+        public bool IsFacingPlayer()
+        {
+            var xDiff = _playerLocation.X - Location.X;
+
+            return xDiff > 0 ? _facing == Direction.Right : _facing == Direction.Left;
+        }
+
+        public void UpdateAction()
+        {
+            _agentStatus = AgentStateUtility.RandomFrom(ValidAgentStates);
+            if (_agentStatus == AgentState.Moving)
+            {
+                _currentDirection = DirectionUtility.GetDirectionTowardsPoint(Location, _playerLocation);
+            }
+            _agentClock = ActionDelay;
+        }
+
+        private void ExecuteAction()
+        {
+            if (_agentClock > 0)
+            {
+                _agentClock--;
+            }
+
+            switch (_agentStatus)
+            {
+                case AgentState.Ready:
+                    UpdateAction();
+                    break;
+                case AgentState.Halted:
+                    if (_agentClock == 0)
+                    {
+                        _agentStatus = AgentState.Ready;
+                    }
+
+                    break;
+                case AgentState.Knocked:
+                    if (_agentClock == 0)
+                    {
+                        _agentStatus = AgentState.Ready;
+                    }
+                    else
+                    {
+                        Move(DirectionUtility.Flip(_currentDirection));
+                    }
+
+                    break;
+                case AgentState.Moving:
+                    if (_agentClock == 0)
+                    {
+                        _agentStatus = AgentState.Ready;
+                    }
+                    else
+                    {
+                        Move(_currentDirection);
+                    }
+
+                    break;
+                case AgentState.Attacking:
+                    if (_agentClock == 0)
+                    {
+                        if (IsFacingPlayer())
+                        {
+                            UseAttack();
+                        }
+                        
+                        _agentStatus = AgentState.Ready;
+                    }
+
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        private void UseAttack()
+        {
+            const double velocityScalar = 1.5;
+
+            Projectiles.Add(new Fireball(Location, GenerateFireballVector(velocityScalar, 0), true));
+        }
+
+        private Vector2 GenerateFireballVector(double xVelocity, double yVelocityOffset)
+        {
+            double xDiff = _playerLocation.X - Location.X;
+            double yDiff = _playerLocation.Y - Location.Y;
+            var magnitude = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+
+            var normalizedY = yDiff / magnitude;
+
+            return new Vector2((float)xVelocity * Math.Sign(xDiff), (float)(yVelocityOffset + normalizedY));
+        }
+
+        public override void Target(Point playerLocation)
+        {
+            _playerLocation = playerLocation;
+        }
+
+        public override void Update()
+        {
+            if (Alive && CanMove)
+                ExecuteAction();
+
+            base.Update();
+        }
+
+        public override ICommand ProjectileEffect(IProjectile projectile)
+        {
+            return NoOp.Instance;
+        }
+    }
+}
