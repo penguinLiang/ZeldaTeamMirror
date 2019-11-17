a7c5a9e2914a0f4854753ed21375ad4023060cde
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-11-17 10:53:38 -0500

    steven wtf v2

--- ZeldaTeam3/Survival/Wave.cs
+++ ZeldaTeam3/Survival/Wave.cs
@@ -22,19 +22,10 @@ namespace Zelda.Survival
     {
         private List<IEnemy> _waveEnemies = new List<IEnemy>();
 
-        /*
-        public int currentSpawnTimer;
-        public int waveTime;
-        */
-
         public WaveType Type;
-        //public int difficultyScale;
 
         public Wave(List<IEnemy> enemyCSVContent, WaveType waveType)
         {
-            //waveTime = spawnTimer;
-            //currentSpawnTimer = waveTime;
-            //difficultyScale = 1;
             Type = waveType;
 
             foreach (var enemy in enemyCSVContent)
@@ -43,62 +34,17 @@ namespace Zelda.Survival
             }
         }
 
-        public List<IEnemy> getList(int scale)
+        public List<IEnemy> GetList(int scale)
         {
             List<IEnemy> _unspawnedEnemies = new List<IEnemy>();
-            int i = 0;
-            foreach(var enemy in _waveEnemies)
+            for(int i = 0; i < scale; i++)
             {
-                while(i < scale)
+                foreach(var enemy in _waveEnemies)
                 {
                     _unspawnedEnemies.Add(enemy);
-                    i++;
                 }
-                i = 0;
             }
             return _unspawnedEnemies;
         }
     }
 }        
-/*
-        public int CanSpawnEnemy()
-        {
-            // There is still enemies left, and spawn timer is ready to spawn, return 0
-            if(currentSpawnTimer == 0 && UnspawnedEnemies.Count != 0)
-            {
-                currentSpawnTimer = waveTime;
-                return 0;
-            }
-
-            // There is no more enemies, the wave is over, so return 1
-            if(UnspawnedEnemies.Count == 0)
-            {
-                return 1;
-            }
-
-            // There are still enemies, but spawn is still on cooldown, return 2
-            return 2;
-            
-        }*/
-
-        /*
-        public void Update()
-        {
-            if(UnspawnedEnemies.Count == 0)
-            {
-                difficultyScale++;
-                foreach (var enemy in WaveEnemies)
-                {
-                    int i = 0;
-                    while (i < difficultyScale)
-                    {
-                        UnspawnedEnemies.Add(enemy);
-                        i++;
-                    }
-                }
-            }
-            if(currentSpawnTimer > 0)
-            {
-                currentSpawnTimer--;
-            }
-        } */
