1a8434ed8c6685e1357d8af92b3d203a99338c6b
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-11-17 13:20:05 -0500

    Parsers and etc made

---
+++ CSVMapContentPipeline/CSVWaveReader.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+// ReSharper disable UnusedMember.Global (used by pipeline as DLL)
+namespace CSVMapContentPipeline
+{
+    internal class CSVWaveReader : ContentTypeReader<string[][]>
+    {
+        protected override string[][] Read(ContentReader input, string[][] existingInstance)
+        {
+            var rows = input.ReadInt32();
+            var cols = input.ReadInt32();
+            var result = new string[rows][];
+
+            for (var row = 0; row < rows; row++)
+            {
+                result[row] = new string[cols];
+                for (var col = 0; col < cols; cols++)
+                {
+                    result[row][col] = input.ReadString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
