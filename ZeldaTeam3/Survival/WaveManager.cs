9ffe06d3b7bd48c871f09eea56e25a2bcde585bc
Foodshops <32028050+Foodshops@users.noreply.github.com>
2019-11-17 17:55:13 -0500

    Changed all per Stevens request

--- ZeldaTeam3/Survival/WaveManager.cs
+++ ZeldaTeam3/Survival/WaveManager.cs
@@ -32,65 +32,26 @@ namespace Zelda.Survival
             char[] separator = {':'};
             Int32 count = 2;
 
-            for(int i = 0; i < _waveMatrix.Length; i++)
+            for(int row = 0; row < _waveMatrix.Length; row++)
             {
                 WaveType currentWaveType = WaveType.Normal;
                 List<EnemyType> enemyCSVContent = new List<EnemyType>();
-                for(int j = 0; j < _waveMatrix[i].Length; j++)
+                for(int col = 0; col < _waveMatrix[row].Length; col++)
                 {
-                    if(j == 0)
+                    if(col == 0)
                     {
-                        switch(_waveMatrix[i][0])
-                        {
-                            case "S":
-                                currentWaveType = WaveType.Shop;
-                                break;
-                            case "P":
-                                currentWaveType = WaveType.Party;
-                                break;
-                            case "D":
-                                currentWaveType = WaveType.Normal;
-                                break;
-                            default:
-                                throw new Exception("Needs a valid wave type!");
-                        }
+                        currentWaveType = _getCurrentWaveType(row);
                     }
-
                     else
                     {
-                        String[] strList = _waveMatrix[i][j].Split(separator, count, StringSplitOptions.None);
+                        String[] strList = _waveMatrix[row][col].Split(separator, count, StringSplitOptions.None);
                         int enemyCount = int.Parse(strList[1]);
-                        EnemyType EnemyType = EnemyType.Gel;
-                        switch(strList[0])
-                        {
-                            case "aquamentus":
-                                EnemyType = EnemyType.Aquamentus;
-                                break;
-                            case "gel":
-                                EnemyType = EnemyType.Gel;
-                                break;
-                            case "goriya":
-                                EnemyType = EnemyType.Goriya;
-                                break;
-                            case "keese":
-                                EnemyType = EnemyType.Keese;
-                                break;
-                            case "stalfos":
-                                EnemyType = EnemyType.Stalfos;
-                                break;
-                            case "wallmaster":
-                                EnemyType = EnemyType.WallMaster;
-                                break;
-                            case "fygar":
-                                EnemyType = EnemyType.Fygar;
-                                break;
-                            default:
-                                throw new Exception("Needs to be a valid enemy type!");
-                        }
 
-                        for(int k = 0; k < enemyCount; k++)
+                        EnemyType enemyType = _getCurrentEnemyType(row, col, strList);
+
+                        for(int currentCount = 0; currentCount < enemyCount; currentCount++)
                         {
-                            enemyCSVContent.Add(EnemyType);
+                            enemyCSVContent.Add(enemyType);
                         }
                     }
                 }
@@ -100,6 +61,57 @@ namespace Zelda.Survival
             }
         }
 
+        private WaveType _getCurrentWaveType(int row)
+        {
+            WaveType currentWaveType = WaveType.Normal;
+            switch (_waveMatrix[row][0])
+            {
+                case "S":
+                    currentWaveType = WaveType.Shop;
+                    break;
+                case "P":
+                    currentWaveType = WaveType.Party;
+                    break;
+                case "D":
+                    currentWaveType = WaveType.Normal;
+                    break;
+                default:
+                    throw new Exception("Needs a valid wave type!");
+            }
+            return currentWaveType;
+        }
+
+        private EnemyType _getCurrentEnemyType(int row, int col, string[] strList)
+        {
+            EnemyType enemyType = EnemyType.None;
+            switch (strList[0])
+            {
+                case "aquamentus":
+                    enemyType = EnemyType.Aquamentus;
+                    break;
+                case "gel":
+                    enemyType = EnemyType.Gel;
+                    break;
+                case "goriya":
+                    enemyType = EnemyType.Goriya;
+                    break;
+                case "keese":
+                    enemyType = EnemyType.Keese;
+                    break;
+                case "stalfos":
+                    enemyType = EnemyType.Stalfos;
+                    break;
+                case "wallmaster":
+                    enemyType = EnemyType.WallMaster;
+                    break;
+                case "fygar":
+                    enemyType = EnemyType.Fygar;
+                    break;
+                default:
+                    throw new Exception("Needs to be a valid enemy type!");
+            }
+            return enemyType;
+        }
 
     }
 }
