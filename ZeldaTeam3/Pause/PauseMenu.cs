96f9753eba9c22dbd21a99f6a7c1eec4692f6c9b
Chase Colman <chase@colman.io>
2019-11-29 00:29:43 -0500

    Move Pause/HUD modifications to Survival pre-merge

--- ZeldaTeam3/Pause/PauseMenu.cs
+++ ZeldaTeam3/Pause/PauseMenu.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Zelda.Commands;
 using Zelda.GameState;
 using Zelda.Items;
-// ReSharper disable ConvertIfStatementToSwitchStatement
 
 namespace Zelda.Pause
 {
@@ -12,20 +12,6 @@ namespace Zelda.Pause
         private readonly Vector2 _location;
         private Point _cursorPosition;
         private ISprite _selectedItem;
-        private ISprite _slot7Sprite;
-        private ISprite _slot8Sprite;
-
-        private static ISprite GetExtraItemSprite(Secondary extraItem)
-        {
-
-            switch (extraItem)
-            {
-                case Secondary.LaserBeam:
-                    return LaserBeam;
-                default:
-                    return null;
-            }
-        }
 
         public PauseMenu(GameStateAgent agent, Point location)
         {
@@ -33,6 +19,12 @@ namespace Zelda.Pause
             _location = location.ToVector2();
             switch (agent.Player.Inventory.SecondaryItem)
             {
+                case Secondary.Bow when agent.Player.Inventory.HasArrow && agent.Player.Inventory.HasBow:
+                    _selectedItem = Arrow;
+                    _cursorPosition = BowPosition;
+                    break;
+                case Secondary.Bow:
+                    break;
                 case Secondary.Boomerang:
                     _selectedItem = Boomerang;
                     _cursorPosition = BoomerangPosition;
@@ -41,41 +33,6 @@ namespace Zelda.Pause
                     _selectedItem = Bomb;
                     _cursorPosition = BombPosition;
                     break;
-                case Secondary.Bow when agent.Player.Inventory.ArrowLevel != Secondary.None:
-                case Secondary.FireBow when agent.Player.Inventory.ArrowLevel != Secondary.None:
-                    _selectedItem = Arrow;
-                    _cursorPosition = BowPosition;
-                    break;
-                case Secondary.Coins:
-                    _selectedItem = AlchemyCoin;
-                    _cursorPosition = CoinPosition;
-                    break;
-                case Secondary.ATWBoomerang:
-                    _selectedItem = ATWBoomerang;
-                    _cursorPosition = ATWBoomerangPosition;
-                    break;
-                case Secondary.BombLauncher:
-                    _selectedItem = BombLauncher;
-                    _cursorPosition = BombLauncherPosition;
-                    break;
-                case Secondary.ExtraSlot1:
-                    _selectedItem = GetExtraItemSprite(_agent.Player.Inventory.ExtraItem1);
-                    _cursorPosition = Slot7Position;
-                    break;
-                case Secondary.ExtraSlot2:
-                    _selectedItem = GetExtraItemSprite(_agent.Player.Inventory.ExtraItem2);
-                    _cursorPosition = Slot8Position;
-                    break;
-                case Secondary.None:
-                    break;
-                case Secondary.Arrow:
-                    break;
-                case Secondary.SilverArrow:
-                    break;
-                case Secondary.LaserBeam:
-                    break;
-                case Secondary.Bait:
-                    break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
@@ -83,34 +40,14 @@ namespace Zelda.Pause
 
         public void Update()
         {
-            switch (_agent.Player.Inventory.ExtraItem1)
-            {
-                case Secondary.LaserBeam:
-                    _slot7Sprite = LaserBeam;
-                    break;
-                default:
-                    _slot7Sprite = null;
-                    break;
-            }
-            switch (_agent.Player.Inventory.ExtraItem2)
-            {
-                case Secondary.LaserBeam:
-                    _slot8Sprite = LaserBeam;
-                    break;
-                default:
-                    _slot8Sprite = null;
-                    break;
-            }
             CursorGrid.Update();
         }
 
         public void Draw()
         {
             Background.Draw(_location);
-            if (_agent.Player.Inventory.SecondaryItem == Secondary.Coins)
-                _selectedItem?.Draw(_location + SelectedItemLocation16_16);
-            else
-                _selectedItem?.Draw(_location + SelectedItemLocation8_16);
+            CursorGrid.Draw(CursorSize * _cursorPosition.ToVector2() + GridLocation + _location);
+            _selectedItem?.Draw(_location + SelectedItemLocation);
 
             var currentRoom = _agent.DungeonManager.CurrentRoom.ToVector2();
             var visitedRooms = _agent.DungeonManager.VisitedRooms;
@@ -128,92 +65,51 @@ namespace Zelda.Pause
                 }
             }
 
-            if (_agent.Player.Inventory.HasBoomerang)
-                Boomerang.Draw(BoomerangLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.BombCount >= 1)
-                Bomb.Draw(BombLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.ArrowLevel == Secondary.SilverArrow)
-                SilverArrow.Draw(ArrowLocation + GridLocation + _location);
-            else if (_agent.Player.Inventory.ArrowLevel == Secondary.Arrow)
-                Arrow.Draw(ArrowLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.BowLevel == Secondary.FireBow)
-                FireBow.Draw(BowLocation + GridLocation + _location);
-            else if (_agent.Player.Inventory.BowLevel == Secondary.Bow)
-                Bow.Draw(BowLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.Coins >= 2)
-                AlchemyCoin.Draw(CoinLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.HasATWBoomerang)
-                ATWBoomerang.Draw(ATWBoomerangLocation + GridLocation + _location);
-
-            if (_agent.Player.Inventory.HasBombLauncher)
-                BombLauncher.Draw(BombLauncherLocation + GridLocation + _location);
-
-            _slot7Sprite?.Draw(Slot7Location + GridLocation + _location);
-            _slot8Sprite?.Draw(Slot8Location + GridLocation + _location);
-
             if (_agent.Player.Inventory.HasMap)
+            {
                 Map.Draw(MapLocation + _location);
+            }
             if (_agent.Player.Inventory.HasCompass)
+            {
                 Compass.Draw(CompassLocation + _location);
-
-            CursorGrid.Draw(CursorSize * _cursorPosition.ToVector2() + GridLocation + _location);
+            }
+            if (_agent.Player.Inventory.HasArrow)
+            {
+                Arrow.Draw(ArrowLocation + GridLocation + _location);
+            }
+            if (_agent.Player.Inventory.HasBow)
+            {
+                Bow.Draw(BowLocation + GridLocation + _location);
+            }
+            if (_agent.Player.Inventory.HasBoomerang)
+            {
+                Boomerang.Draw(BoomerangLocation + GridLocation + _location);
+            }
+            if (_agent.Player.Inventory.BombCount >= 1)
+            {
+                Bomb.Draw(BombLocation + GridLocation + _location);
+            }
         }
 
         private void AssignSecondary()
         {
-            Secondary item;
+            ICommand assign = new NoOp();
             if (_cursorPosition == BoomerangPosition && _agent.Player.Inventory.HasBoomerang)
             {
-                item = Secondary.Boomerang;
+                assign = new LinkSecondaryAssign(_agent.Player, Secondary.Boomerang);
                 _selectedItem = Boomerang;
             }
-            else if (_cursorPosition == BombPosition && _agent.Player.Inventory.BombCount >= 1)
+            if (_cursorPosition == BombPosition && _agent.Player.Inventory.BombCount >= 1)
             {
-                item = Secondary.Bomb;
+                assign = new LinkSecondaryAssign(_agent.Player, Secondary.Bomb);
                 _selectedItem = Bomb;
             }
-            else if (_cursorPosition == BowPosition && _agent.Player.Inventory.BowLevel != Secondary.None
-                && _agent.Player.Inventory.ArrowLevel != Secondary.None)
+            if (_cursorPosition == BowPosition && _agent.Player.Inventory.HasBow && _agent.Player.Inventory.HasArrow)
             {
-                item = _agent.Player.Inventory.BowLevel == Secondary.Bow ? Secondary.Bow : Secondary.FireBow;
-                _selectedItem = _agent.Player.Inventory.ArrowLevel == Secondary.Arrow ? Arrow : SilverArrow;
+                assign = new LinkSecondaryAssign(_agent.Player, Secondary.Bow);
+                _selectedItem = Arrow;
             }
-            else if (_cursorPosition == CoinPosition && _agent.Player.Inventory.Coins >= 2)
-            {
-                item = Secondary.Coins;
-                _selectedItem = AlchemyCoin;
-            }
-            else if (_cursorPosition == ATWBoomerangPosition && _agent.Player.Inventory.HasATWBoomerang)
-            {
-                item = Secondary.ATWBoomerang;
-                _selectedItem = ATWBoomerang;
-            }
-            else if (_cursorPosition == BombLauncherPosition && _agent.Player.Inventory.HasBombLauncher)
-            {
-                item = Secondary.BombLauncher;
-                _selectedItem = BombLauncher;
-            }
-            else if (_cursorPosition == Slot7Position)
-            {
-                item = Secondary.ExtraSlot1;
-                _selectedItem = _slot7Sprite;
-            }
-            else if (_cursorPosition == Slot8Position)
-            {
-                item = Secondary.ExtraSlot2;
-                _selectedItem = _slot8Sprite;
-            }
-            else
-            {
-                item = Secondary.None;
-                _selectedItem = null;
-            }
-            _agent.Player.AssignSecondaryItem(item);
+            assign.Execute();
         }
 
         public void Choose()
---
+++ ZeldaTeam3/Survival/Pause/PauseMenu.Constants.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Zelda.Items;
+
+namespace Zelda.Pause
+{
+    public partial class PauseMenu
+    {
+        private const int InventoryGridRows = 2;
+        private const int InventoryGridColumns = 4;
+
+        private static readonly Vector2 GridLocation = new Vector2(128, 40);
+        private static readonly Vector2 CursorSize = new Vector2(24, 16);
+        private static readonly Vector2 SelectedItemLocation8_16 = new Vector2(68, 40);
+        private static readonly Vector2 SelectedItemLocation16_16 = new Vector2(64, 40);
+
+        private static readonly Vector2 MapGridLocation = new Vector2(136, 104);
+        private static readonly Vector2 MapGridCoverSize = new Vector2(8, 8);
+        private static readonly ISprite PlayerMapDot = PauseSpriteFactory.Instance.CreateLinkIndicator();
+        private static readonly ISprite RoomCover = PauseSpriteFactory.Instance.CreateMapCoverSquare();
+
+        private static readonly Point BoomerangPosition = new Point(0, 0);
+        private static readonly Point BombPosition = new Point(1, 0);
+        private static readonly Point BowPosition = new Point(2, 0);
+        private static readonly Point CoinPosition = new Point(3, 0);
+        private static readonly Point ATWBoomerangPosition = new Point(0, 1);
+        private static readonly Point BombLauncherPosition = new Point(1, 1);
+        private static readonly Point Slot7Position = new Point(2, 1);
+        private static readonly Point Slot8Position = new Point(3, 1);
+
+        private static readonly Vector2 BoomerangLocation = new Vector2(4, 0);
+        private static readonly Vector2 BombLocation = new Vector2(28, 0);
+        private static readonly Vector2 ArrowLocation = new Vector2(48, 0);
+        private static readonly Vector2 BowLocation = new Vector2(56, 0);
+        private static readonly Vector2 CoinLocation = new Vector2(72, 0);
+        private static readonly Vector2 ATWBoomerangLocation = new Vector2(4, 16);
+        private static readonly Vector2 BombLauncherLocation = new Vector2(28, 16);
+        private static readonly Vector2 Slot7Location = new Vector2(52, 16);
+        private static readonly Vector2 Slot8Location = new Vector2(76, 16);
+
+        private static readonly ISprite Boomerang = ItemSpriteFactory.Instance.CreateWoodBoomerang();
+        private static readonly ISprite Bomb = ItemSpriteFactory.Instance.CreateBomb();
+        private static readonly ISprite Arrow = ItemSpriteFactory.Instance.CreateArrow();
+        private static readonly ISprite SilverArrow = ItemSpriteFactory.Instance.CreateSilverArrow();
+        private static readonly ISprite Bow = ItemSpriteFactory.Instance.CreateBow();
+        private static readonly ISprite FireBow = ItemSpriteFactory.Instance.CreateFireBow();
+        private static readonly ISprite AlchemyCoin = ItemSpriteFactory.Instance.CreateAlchemyCoin();
+        private static readonly ISprite ATWBoomerang = ItemSpriteFactory.Instance.CreateATWBoomerang();
+        private static readonly ISprite BombLauncher = ItemSpriteFactory.Instance.CreateBombLauncher();
+        private static readonly ISprite LaserBeam = ItemSpriteFactory.Instance.CreateLaserBeam();
+
+        private static readonly Vector2 CompassLocation = new Vector2(44, 144);
+        private static readonly Vector2 MapLocation = new Vector2(48, 104);
+        private static readonly ISprite Compass = ItemSpriteFactory.Instance.CreateCompass();
+        private static readonly ISprite Map = ItemSpriteFactory.Instance.CreateMap();
+
+        private static readonly ISprite Background = PauseSpriteFactory.Instance.CreateBackground();
+        private static readonly ISprite CursorGrid = PauseSpriteFactory.Instance.CreateCursorFrame();
+    }
+}
