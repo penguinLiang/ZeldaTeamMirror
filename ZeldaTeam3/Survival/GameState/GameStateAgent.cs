c90dd9b5836b5b8649852eca90d8caa02161ccfe
Chase Colman <chase@colman.io>
2019-12-02 16:10:54 -0500

    Start in dungeon

--- ZeldaTeam3/Survival/GameState/GameStateAgent.cs
+++ ZeldaTeam3/Survival/GameState/GameStateAgent.cs
@@ -73,7 +73,7 @@ namespace Zelda.Survival.GameState
             Player.Spawn();
             Player.MaxHealth = 12;
             Player.FullHeal();
-            DungeonManager.JumpToRoom(0, 0);
+            DungeonManager.JumpToRoom(1, 0);
             Play();
         }
 
